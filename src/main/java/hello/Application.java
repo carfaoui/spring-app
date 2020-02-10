641d1eff75ece7a29ca6eca499b482421226ea9e
chaima arfaoui <chaima.arfaoui.ext@orange.com>
2020-02-10 15:38:03 +0100

    update

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
 
 
-    private String feature_2
+    private String feature_2 ;
 
 
     @RequestMapping("/")
